f35ae4eba2726d9fb2eb0ba24945e8eb32e6a589
inki.donz <inki.donz@gmail.com>
2025-07-22 10:34:05 +0000

    firebase update

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDNY67dtYOw5z8rDqs_7rfSixsMDDukQEw",
   authDomain: "grafikkalinowa.firebaseapp.com",
-  databaseURL: "https://grafikkalinowa-default-rtdb.europe-west1.firebasedatabase.app",
+  // USUNIĘTO NIEPOPRAWNY KLUCZ databaseURL
   projectId: "grafikkalinowa",
-  storageBucket: "grafikkalinowa.firebasestorage.app",
+  storageBucket: "grafikkalinowa.appspot.com", // Poprawiono również na standardowy format, bez "firebase" w środku
   messagingSenderId: "531819524737",
   appId: "1:531819524737:web:bb3f279ef99419095e1380",
   measurementId: "G-5X744M8VG5"
