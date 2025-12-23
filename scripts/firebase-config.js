1c2d5748faeaaa5993786ba2725b6f6429a957af
FozjoterapiaKalino <fizjoterapiakalino@gmail.com>
2025-12-23 17:37:36 +0100

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -12,6 +12,16 @@ const firebaseConfig = {
   appId: '1:531819524737:web:bb3f279ef99419095e1380',
   measurementId: 'G-5X744M8VG5',
 };
+// Właściwa baza danych
+//const firebaseConfig = {
+//  apiKey: "AIzaSyCdPhCgZeFYv3fLrd9Xc4AVwBu70cCvlVQ",
+//  authDomain: "grafikkalinowa-c1b41.firebaseapp.com",
+//  projectId: "grafikkalinowa-c1b41",
+//  storageBucket: "grafikkalinowa-c1b41.firebasestorage.app",
+//  messagingSenderId: "59665168961",
+//  appId: "1:59665168961:web:166b1816b1981b2babe4c0",
+//  measurementId: "G-RXBFWH2CXN"
+//};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
