a1ce6f0b012b50888b58406c02c632b9d3708118
FozjoterapiaKalino <fizjoterapiakalino@gmail.com>
2025-12-30 13:18:52 +0100

    Switch to production Firebase database

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -34,16 +34,15 @@ import {
     createUserWithEmailAndPassword,
 } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 
-// Testowa Baza Danych
+// Produkcyjna Baza Danych
 const firebaseConfig = {
-  apiKey: 'AIzaSyDNY67dtYOw5z8rDqs_7rfSixsMDDukQEw',
-  authDomain: 'grafikkalinowa.firebaseapp.com',
-  databaseURL: 'https://grafikkalinowa-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'grafikkalinowa',
-  storageBucket: 'grafikkalinowa.firebasestorage.app',
-  messagingSenderId: '531819524737',
-  appId: '1:531819524737:web:bb3f279ef99419095e1380',
-  measurementId: 'G-5X744M8VG5',
+    apiKey: 'AIzaSyCdPhCgZeFYv3fLrd9Xc4AVwBu70cCvlVQ',
+    authDomain: 'grafikkalinowa-c1b41.firebaseapp.com',
+    projectId: 'grafikkalinowa-c1b41',
+    storageBucket: 'grafikkalinowa-c1b41.firebasestorage.app',
+    messagingSenderId: '59665168961',
+    appId: '1:59665168961:web:166b1816b1981b2babe4c0',
+    measurementId: 'G-RXBFWH2CXN',
 };
 
 // Inicjalizacja Firebase
